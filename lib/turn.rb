1088fd0096c47321d4ccf6a071cdc2dbe7c1c1a7
Angelo Poole <angelo.poole@outlook.com>
2019-11-26 11:19:32 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,14 +8,17 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
 def input_to_index(user_input)
   user_input.to_i - 1
 end
 
 def valid_move?(board, index)
-  if index.between(0,8) && !position_taken?(board, index)
+  if
+    index.between(0,8) && !position_taken?(board, index)
   end
 end
+
 def position_taken?(board, index)
   board[index] != " "
 end
